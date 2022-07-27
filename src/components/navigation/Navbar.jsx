391a558365369a3b2d9ce7881bf0a2fcf0684627
dimasrzkm <dimas1.mahendra@gmail.com>
2022-07-27 13:12:09 +0700

    fix typo in navbar component

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.scss";
+import "./navbar.scss";
 
 export default function Navbar({ pageActive }) {
   const [open, setOpen] = useState(false);
